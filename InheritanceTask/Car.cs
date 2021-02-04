0f675a685c41d6741047a520f19e677268962432
dmitri_shepelev <shepelev.dm@gmail.com>
2021-02-05 02:19:35 +0300

    Done

---
+++ InheritanceTask/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceTask
+{
+    class Car : Vehicle
+    {
+        public Car(string name, int maxSpeed) : base(name, maxSpeed)
+        {
+        }
+
+        public void SetName(string value)
+        {
+            Name = value;
+        }
+
+        public string GetName()
+        {
+            return Name;
+        }
+    }
+}
---
+++ InheritanceTask/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceTask
+{
+    class Vehicle
+    {
+        private readonly string name;
+        private readonly int maxSpeed;
+
+        protected string Name { get; set; }
+        public int MaxSpeed { get; }
+
+        public Vehicle(string name, int maxSpeed)
+        {
+            this.name = name;
+            this.maxSpeed = maxSpeed;
+        }
+    }
+}
